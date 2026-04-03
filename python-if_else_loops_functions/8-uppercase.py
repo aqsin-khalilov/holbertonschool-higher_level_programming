85d0db123768408d73f5fec8423afbf7052c2daf
aqsin-khalilov <aqsinnxalilov@gmail.com>
2026-04-03 18:38:14 +0000

    To uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        # Herfin kiÃ§ik olub-olmadgÄ±±nÄ± yoxlayÄ
         temp_char = char
         if ord(char) >= ord('a') and ord(char) <= ord('z'):
             temp_char = chr(ord(char) - 32)
