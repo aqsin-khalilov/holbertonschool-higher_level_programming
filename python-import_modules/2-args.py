26fa1985337727c44c98e42904f2a1ec9113aeb6
aqsin-khalilov <aqsinnxalilov@gmail.com>
2026-04-03 20:56:14 +0000

    How to make a script dynamic!

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    #We count the number of arguments to remove the name of the program
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(count))
+    
+    for i in range(1, count + 1):
+        print("{:d}: {}".format(i, sys.argv[i]))
