6598fca68bf293fd4eac904975d88c4b3e02df8f
aqsin-khalilov <aqsinnxalilov@gmail.com>
2026-04-11 18:33:24 +0000

    Access and update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Bu modul Kvadrat (Square) sinfi üçün Property (Getter/Setter)
+məntiqini təqdim edir.
+"""
+
+
+class Square:
+    """
+    Kvadratı təmsil edən sinif.
+
+    Atributlar:
+        __size (int): Kvadratın tərəfinin ölçüsü (private).
+    """
+
+    def __init__(self, size=0):
+        """
+        Yeni bir Kvadrat obyekti yaradır.
+
+        Args:
+            size (int): Kvadratın ölçüsü. Susmaya görə 0-dır.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Kvadratın ölçüsünü götürmək üçün Getter metodu.
+
+        Returns:
+            int: Kvadratın ölçüsü.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Kvadratın ölçüsünü təyin etmək üçün Setter metodu.
+
+        Args:
+            value (int): Yeni ölçü dəyəri.
+
+        Raises:
+            TypeError: Əgər value integer deyilsə.
+            ValueError: Əgər value 0-dan kiçikdirsə.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Kvadratın sahəsini hesablayır.
+
+        Returns:
+            int: Kvadratın sahəsi.
+        """
+        return (self.__size * self.__size)
