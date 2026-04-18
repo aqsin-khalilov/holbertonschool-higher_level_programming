a6fd7d99c06c57c148f8f6b71fe7fd0ed91ff3d8
aqsin-khalilov <aqsinnxalilov@gmail.com>
2026-04-18 09:23:27 +0000

    Select the best

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- List records with score >= 10
+-- This script lists records with score and name, ordered by score (top first)
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
