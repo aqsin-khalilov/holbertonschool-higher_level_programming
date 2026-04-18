5bb7b39f743c7144d23783a1a30c2423ef42f62b
aqsin-khalilov <aqsinnxalilov@gmail.com>
2026-04-18 09:21:40 +0000

    List by best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- List all records of second_table by score
+-- This script lists records with score and name, ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
