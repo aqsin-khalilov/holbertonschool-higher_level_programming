cbabc12eb624082e4678041e1d75282869b7a0c4
aqsin-khalilov <aqsinnxalilov@gmail.com>
2026-04-25 14:01:11 +0000

    What's my status? #1

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script fetches a specific URL using the requests package
+and displays the body of the response with specific formatting.
+"""
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    
+    # Custom headers to bypass the intranet firewall
+    headers = {'cfclearance': 'true'}
+    
+    # Sending the GET request
+    response = requests.get(url, headers=headers)
+    
+    # Getting the text content of the response
+    content = response.text
+    
+    print("Body response:")
+    # requests automatically decodes the response into a string
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
